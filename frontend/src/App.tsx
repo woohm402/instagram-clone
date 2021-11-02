ed6cdf471744dbc544253d08274097dd68c4f006
woohm402 <woohm404@gmail.com>
2021-11-02 21:32:40 +0900

    route 초기 구조

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+
 const App: React.FC = () => {
-  return <div className="App">Init</div>;
+  return (
+    <>
+      <header>TODO header</header>
+      <BrowserRouter>
+        <Route exact path={'/'} component={() => null} />
+        <Redirect to={'/'} />
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
